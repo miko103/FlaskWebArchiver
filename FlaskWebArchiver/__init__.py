4e8ab2ba4c3ca44146992446c0bcaa02d6f9f1b8
miki <84585119+miko1034@users.noreply.github.com>
2024-11-25 10:01:22 +0000

    cfg works, redid db creation, cleaned code

--- FlaskWebArchiver/__init__.py
+++ FlaskWebArchiver/__init__.py
@@ -12,60 +12,53 @@ import yaml
 
 def start():
     
-    f = open("cfg.yml", "w")
-    f.write(f"""SECRET_KEY: '{"".join(random.choices(string.ascii_letters, k=20))}'
-database_name: 'database.db' # default: database.cfg
-MAIL_ACCOUNT: ''
-MAIN_PASS: ''""")
-    f.close()
+    if "cfg.yml" not in os.listdir():
+        f = open("cfg.yml", "w")
+        f.write(f"""SECRET_KEY: '{"".join(random.choices(string.ascii_letters, k=20))}'
+                database_name: 'database.db' # default: database.db
+                MAIL_ACCOUNT: ''
+                MAIN_PASS: ''""")
+        f.close()
 
     with open('cfg.yml', 'r') as cfg:
         settings = yaml.safe_load(cfg)
         MAIN_DB_NAME = settings["database_name"]
 
-    if MAIN_DB_NAME not in os.listdir():
-        # creates table for user data
-        db = open(f'./{MAIN_DB_NAME}', 'w+')
-        db.close()
+    
+    # creates table for user data
+    con = sqlite3.connect(MAIN_DB_NAME)
+    cursor = con.cursor()
 
-        con = sqlite3.connect(MAIN_DB_NAME)
-        cursor = con.cursor()
-
-        cursor.execute("""CREATE TABLE stats(
-                   stat_id INTEGER PRIMARY KEY ,
-                   total_searches INT,
-                   total_saves INT
+    cursor.execute("""CREATE TABLE IF NOT EXISTS stats(
+               stat_id INTEGER PRIMARY KEY ,
+               total_searches INT,
+               total_saves INT
+               )""")
+    #creates main userdata table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS userdata(
+                   uid INTEGER PRIMARY KEY,
+                   username TEXT NOT NULL,
+                   phash TEXT NOT NULL,
+                   email TEXT NOT NULL,
+                   stat_id INT NOT NULL,
+                   vercode INT,
+                   FOREIGN KEY(stat_id) REFERENCES stats(stat_id)
                    )""")
-
-        #creates main userdata table
-        cursor.execute("""CREATE TABLE userdata(
-                       uid INTEGER PRIMARY KEY,
-                       username TEXT NOT NULL,
-                       phash TEXT NOT NULL,
-                       email TEXT NOT NULL,
-                       stat_id INT NOT NULL,
-                       vercode INT,
-                       FOREIGN KEY(stat_id) REFERENCES stats(stat_id)
-                       )""")
-
-        cursor.execute("""CREATE TABLE sites(
-                       site_id INTEGER PRIMARY KEY,
-                       scraped_by INT NOT NULL,
-                       FOREIGN KEY(scraped_by) REFERENCES userdata(uid)
-                       )""")
-
-        cursor.execute("""CREATE TABLE sites_data (
-                       site_id INTEGER PRIMARY KEY,
-                       url TEXT NOT NULL,
-                       timestamp RFLOATEAL NOT NULL,
-                       local_path TEXT NOT NULL,
-                       FOREIGN KEY(site_id) REFERENCES sites(site_id)
-                       )""")
-
-
-        con.commit()
-        cursor.close()
-        con.close()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS sites(
+                   site_id INTEGER PRIMARY KEY,
+                   scraped_by INT NOT NULL,
+                   FOREIGN KEY(scraped_by) REFERENCES userdata(uid)
+                   )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS sites_data (
+                   site_id INTEGER PRIMARY KEY,
+                   url TEXT NOT NULL,
+                   timestamp RFLOATEAL NOT NULL,
+                   local_path TEXT NOT NULL,
+                   FOREIGN KEY(site_id) REFERENCES sites(site_id)
+                   )""")
+    con.commit()
+    cursor.close()
+    con.close()
 
     
     # creates folder for website save data
